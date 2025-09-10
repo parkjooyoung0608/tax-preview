e0f2ae9f9b9a5c9af09f04a129804e6ed7f38133
parkjooyoung <mirkduddl@naver.com>
2025-09-11 01:33:25 +0900

    feat: ComputedDeduction Display 컴포넌트 작업

---
+++ src/components/computedDeduction/Display.tsx
@@ -0,0 +1,33 @@
+import Title from "@components/computedDeduction/Title";
+import RefLink from "@components/ui/RefLink";
+import formatKoreanCurrency from "@utils/formatKoreanCurrency";
+import { IComputedDeductionDisplay } from "@interface";
+
+export default function ComputedDeductionDisplay({
+  title,
+  공제금액,
+  공제한도,
+  url,
+  tooltipContent,
+}: IComputedDeductionDisplay) {
+  return (
+    <>
+      <div className="flex justify-between items-start">
+        <div>
+          <Title title={title} tooltipContent={tooltipContent} />
+          {url && <RefLink url={url} />}
+        </div>
+        <div className="text-right">
+          <span className="font-bold text-blue-600">
+            {formatKoreanCurrency(공제금액)}원
+          </span>
+          {공제한도 && (
+            <div className="mt-1 text-xs text-gray-600">
+              최대 공제 한도 {formatKoreanCurrency(공제한도)}원
+            </div>
+          )}
+        </div>
+      </div>
+    </>
+  );
+}
